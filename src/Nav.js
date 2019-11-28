a0aca4675a81f958126735ce99d529cda2112189
Andrew Wu <andrew_0564@hotmail.com>
2019-11-28 18:30:52 -0500

    Home and Nav bar

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    )
+}
